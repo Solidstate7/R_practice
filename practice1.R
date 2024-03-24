b86c9b14fa453bc83666db5dead4822b719f8875
Eunjae Lee <vanitasium@gmail.com>
2024-03-24 14:19:46 +0900

    first commit

---
+++ practice1.R
@@ -0,0 +1,32 @@
+a <- 1 > 2
+b <- 2 < 3
+mode(a)
+c = c(1, NA, 3)
+
+x = c(1,2,3)
+y = c(4,5,6)
+x*y
+x^2
+x**2
+log(x**2)
+z = c(x,y)
+z
+
+1/0
+
+log(-1)
+
+sort(x)
+sum(x)
+mean(x) == sum(x)/length(x)
+
+mean(c, na.rm = TRUE)
+is.na(c)
+which(is.na(c))
+sum(is.na(c))
+
+var(x)
+min(x)
+which.min(x)
+summary(x)
+
---
+++ stat1.R
@@ -0,0 +1,15 @@
+height = c(165, 150,131)
+
+e = seq(1,9,2) # sequence
+e
+
+f = rep(10,5) # repeat
+f
+
+g = c(e,f) # concatenate
+g
+
+h = c(4:1, seq(0,9,3)) # range and sequence concat
+h
+
+e + f # component-wise calculation
